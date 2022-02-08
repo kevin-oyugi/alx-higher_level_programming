6227ca907b60709b5c76082ed741f441c04a3628
kevin-oyugi <kevinxyoyugi@gmail.com>
2022-02-07 19:58:16 -0800

    added public method that returns the area value of a rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -66,3 +66,7 @@ class Rectangle(Base):
             raise ValueError("{} must be >= 0".format(name))
         elif not eq and value <= 0:
             raise ValueError("{} must be > 0".format(name))
+
+    def area(self):
+        '''find area of a rectangle'''
+        return self.width * self.height
